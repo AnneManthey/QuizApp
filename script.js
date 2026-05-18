caeb20c7a26e2d2571ec24afb96bfd3f5c1a682a
Anne Manthey <anne.manthey@pm.me>
2026-05-18 08:09:10 +0200

    add JSON

---
+++ script.js
@@ -0,0 +1,25 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+]
+
+function init(){
+
+    document.getElementById("all-questions").innerHTML = questions.length;
+    
+}
