a052425d1969e5ba30e866cf40cd551b17c4f3fe
Anne Manthey <anne.manthey@pm.me>
2026-05-18 11:22:35 +0200

    add nextQuestion function

--- script.js
+++ script.js
@@ -74,4 +74,24 @@ function answer(selection){
          document.getElementById(selection).parentNode.classList.add('bg-danger');
          document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion(){
+    currentQuestion ++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+    
+}
+
+function resetAnswerButtons(){
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove ('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove ('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove ('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove ('bg-success');
 }
